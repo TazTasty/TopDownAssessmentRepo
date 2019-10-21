18a708026d76f50137fd3a1f88c0043c67fad116
Luke Roby <20.luke.roby@ksd.org>
2019-10-21 10:37:55 -0700

    project

---
+++ TopDownAssessment/Assets/CScripts/CoinCollect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class CoinCollect : MonoBehaviour
+{
+    public int coinpurse = 0;
+    public Text coinText;
+
+    void Start()
+    {
+        coinText.text = "Coins: " + coinpurse;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            coinpurse++;
+            coinText.text = "Coins: " + coinpurse;
+            Destroy(collision.gameObject);
+            
+        }
+
+    }
+}
---
+++ TopDownAssessment/Assets/CScripts/MainMenuScript1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public int lives = 5;
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+        PlayerPrefs.SetInt("Lives", lives);
+    }
+}
